ee1b3f475003d1c9d4bc861e84372f8b40a80529
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2018-04-06 02:59:21 +0600

    Update stringtest.c

--- stringtest.c
+++ stringtest.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 #include"SubString.h"
+#include"INDEX.h"
+#include"Concate.h"
 
 int main()
 {
     char *t="To be or not to be";
+
+    //substr check
     char *s1=SubStr(t,1,5);
-    printf("%s",s1);
+    printf("%s\n\n",s1);
+
+    //index check
+    int k=Index(t,"or");
+    printf("Pattern found at index: %d\n\n",k+1);
+
+    //concatenation check
+    char *a=Concat(t," abc");
+    printf("%s\n\n",a);
+
+    //
 
     return 0;
 }
