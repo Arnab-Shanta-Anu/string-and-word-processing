df73c8ee189b5d5439077100113c8fd8b3903d09
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2018-04-06 12:57:16 +0600

    Update stringtest.c
    added malloc and defined alc as malloc macro

--- stringtest.c
+++ stringtest.c
@@ -5,20 +5,24 @@
 #include"INDEX.h"
 #include"Concate.h"
 
+#define alc(x) ((char *)malloc(x))
+
 int main()
 {
-    char *t="To be or not to be";
-
+    char *t=alc(50);
+    t="To be or not to be\0";
     //substr check
-    char *s1=SubStr(t,1,5);
+    char *s1=alc(50);
+    s1=SubStr(t,1,5);
     printf("%s\n\n",s1);
 
     //index check
-    int k=Index(t,"or");
+    int k=Index(t,"or\0");
     printf("Pattern found at index: %d\n\n",k+1);
 
     //concatenation check
-    char *a=Concat(t," abc");
+    char *a=alc(50);
+    a=Concat(t," abc");
     printf("%s\n\n",a);
 
     //
