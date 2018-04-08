8b1be4156772067a07deac70d161192824732ca0
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2018-04-08 11:37:34 +0600

    Update SubString.h
    faltu

--- SubString.h
+++ SubString.h
@@ -1,9 +1,10 @@
 char *SubStr(char *text, int pos, int len)
 {
-    char *S = malloc(len*sizeof(char));
+    char S[50];
     int i,j=0;
-    for(i=pos-1;i<pos+len;i++){
-        S[j++]=text[i];
+    for(i=pos-1;i<len+pos;i++){
+        S[j]=text[i];
+        j++;
     }
     S[j]='\0';
 
